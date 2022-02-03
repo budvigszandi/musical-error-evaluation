5980ddbcf263951b8114c64d7523ab79ec242db9
Fajtaine Budvig Alexandra <alexandra.budvig@gmail.com>
2022-02-03 10:01:12 +0100

    Deleted old approach, now only using the new approach in evaluating notes

--- evaluate_notes.py
+++ evaluate_notes.py
@@ -1,50 +1,84 @@
 import music21 as m21
 import harmonics
+from itertools import combinations
 
-# Keeping this until evaluate_notes_by_count.py is more ready
+# Idea: check the number of given notes (less, equal, more) by checking
+#       the relationship matrix rows and columns (<, =, >)
 
 # Requires two lists of m21.pitch.Pitch objects
 def compare_notes(expected_notes, given_notes):
   expected_notes_count = len(expected_notes)
-  if expected_notes_count == 1:
-    compare_note_one_to_n(expected_notes[0], given_notes)
-  else:
-    # TODO: what happens if we have more expected notes?
-    print()
+  given_notes_count = len(given_notes)
+  relationship_matrix = [[0 for i in range(expected_notes_count)] for j in range(given_notes_count)] 
+  for i in range(expected_notes_count):
+    for j in range(given_notes_count):
+      relationship_matrix[i][j] = compare_note_pair(given_notes[i], expected_notes[j])
+  # Just testing here
+  for i in range(expected_notes_count):
+    for j in range(given_notes_count):
+      print(f"Z[{i}][{j}]: {given_notes[i]} - {expected_notes[j]} {relationship_matrix[i][j]}")
+    if i < expected_notes_count - 1:
+      print("---")
+  return relationship_matrix
 
-# Requires a m21.pitch.Pitch object and a list of m21.pitch.Pitch objects
-def compare_note_one_to_n(expected_note, given_notes):
-  notes_information = []
-  if len(given_notes) == 0:
-    # TODO: Add mistake point to evaluation answer
-    print("Missing note")
-    notes_information.append((-2, "Missing note"))
-  elif len(given_notes) == 1:
-    note_information = compare_note_one_to_one(expected_note, given_notes[0])
-    notes_information.append(note_information)
-  else:
-    notes_information = get_notes_information_one_to_n(expected_note, given_notes)
-  print_results(expected_note, notes_information, len(given_notes))
+def get_relationship_points(relationship_matrix):
+  rows = len(relationship_matrix)
+  columns = len(relationship_matrix[0])
+  relationship_point_matrix = [[0 for i in range(rows)] for j in range(columns)]
+  for i in range(rows):
+    for j in range(columns):
+      relationship_point_matrix[i][j] = get_current_point(relationship_matrix[i][j][0])
+  # Just testing here
+  for i in range(rows):
+    for j in range(columns):
+      print(f"R[{i}][{j}]: {relationship_point_matrix[i][j]}")
+    if i < rows - 1:
+      print("---")
+  return relationship_point_matrix
 
-# Returns an array of lists with information about the given notes in relation
-# with the expected note. For the meaning of the lists see
-# compare_note_one_to_one(expected_note, given_note)
-#
-# Requires a m21.pitch.Pitch object and a list of m21.pitch.Pitch objects
-def get_notes_information_one_to_n(expected_note, given_notes):
-  notes_information = []
-  for note in given_notes:
-    note_information = compare_note_one_to_one(expected_note, note)
-    notes_information.append(note_information)
-  return notes_information
+def get_current_point(relationship_id):
+  if relationship_id == 0: # Perfect match
+    return 10
+  elif relationship_id == 0.5: # Cent difference
+    return 8
+  elif relationship_id == 1: # Harmonic TODO: Scale through different harmonics
+    return 5
+  elif relationship_id == -1: # Unrelated
+    return -1
 
-# TODO: Modify the evaluation answer according to the new errors, paying
-#       attention to different weights
-# Returns a list with information about the current note in 2-4 elements.
+# Scenario: dictionary of sums with index lists (key: index list, value: sum)
+def get_scenarios(relationship_point_matrix):
+  scenarios = {}
+  rows = len(relationship_point_matrix)
+  columns = len(relationship_point_matrix[0])
+  print(f"{rows} rows {columns} columns")
+  index_variations = get_index_variations(rows, columns)
+  for index_list in list(index_variations):
+    sum = get_sum_of_scenario(index_list, relationship_point_matrix)
+    scenario_tuple = tuple(index_list)
+    scenarios[scenario_tuple] = sum
+  return scenarios
+
+def get_index_variations(rows, columns):
+  indexes = list(range(columns))
+  extended_indexes = []
+  for i in range(rows):
+    extended_indexes += indexes
+  index_variations = list(dict.fromkeys(combinations(extended_indexes, rows)))
+  return index_variations
+
+def get_sum_of_scenario(scenario, relationship_point_matrix):
+  sum = 0
+  for i in range(len(scenario)):
+    sum += relationship_point_matrix[i][scenario[i]]
+  return sum
+
+# Returns a list with information about the current unmatched note pair in
+# 2-4 elements.
 # 1st element: A number that identifies the type of this note in relation with
 #              the expected note.
-#              0: Perfect match, 0.5: Cent difference, 1: Harmonic
-#             -1: Unrelated,      -2: Missing note
+#              0: Perfect match, 0.5: Cent difference, 1: Harmonic, -1: Unrelated
+#              (-2: Missing note <- this can come from other function) TODO: Can it?
 # 2nd element: A string describing the type of this note in relation with the
 #              expected note.
 # 3rd element: The given note. If a note is missing, the list only contains the
@@ -55,78 +89,49 @@ def get_notes_information_one_to_n(expected_note, given_notes):
 #                        (which_harmonic, fundamental_note)
 #
 # Requires two m21.pitch.Pitch objects
-def compare_note_one_to_one(expected_note, given_note):
+def compare_note_pair(given_note, expected_note):
   if expected_note == given_note:
     return (0, "Perfect match", given_note)
   elif expected_note.nameWithOctave == given_note.nameWithOctave:
     return (0.5, "Cent difference", given_note, given_note.microtone.cents - expected_note.microtone.cents)
   else:
-    harmonic_information = harmonics.get_harmonic_info(given_note, expected_note)
-    if harmonic_information != 0:
-      return (1, "Harmonic", given_note, harmonic_information)
+    harmonic_info = harmonics.get_harmonic_info(given_note, expected_note)
+    if harmonic_info != 0:
+      return (1, "Harmonic", given_note, harmonic_info)
     else:
       return (-1, "Unrelated", given_note)
 
-def print_results(expected_note, notes_information, notes_count):
-  # will use notes_count to multiply error weight
-  contains_expected_note = False
-  plus_only_cent_difference = 0
-  plus_harmonics = 0
-  plus_unrelated = 0
-  print("Expected note:", expected_note)
-  print("Given note(s):")
-  for note in notes_information:
-    if note[0] == -1:
-      print(f"{note[2]}: unrelated")
-      plus_unrelated += 1
-    elif note[0] == 0:
-      print(f"{note[2]}: perfect match")
-      contains_expected_note = True
-    elif note[0] == 0.5:
-      print(f"{note[2]}: note match with {note[3]} cent difference")
-      plus_only_cent_difference += 1
-    else:
-      print(f"{note[2]}: {note[3][0]}. harmonic")
-      plus_harmonics += 1
-  
-  if notes_count > 1:
-    if contains_expected_note:
-      print("Contains expected note. The rest:\n"
-            f"{(plus_only_cent_difference / (notes_count - 1)) * 100:.2f}% "
-            f"({plus_only_cent_difference}/{notes_count - 1}) cent difference\n"
-            f"{(plus_harmonics / (notes_count - 1)) * 100:.2f}% "
-            f"({plus_harmonics}/{notes_count - 1}) harmonics\n"
-            f"{(plus_unrelated / (notes_count - 1)) * 100:.2f}% "
-            f"({plus_unrelated}/{notes_count - 1}) unrelated\n")
-    else:
-      print("Contains expected note. The rest:\n"
-            f"{(plus_only_cent_difference / (notes_count)) * 100:.2f}% "
-            f"({plus_only_cent_difference}/{notes_count}) cent difference\n"
-            f"{(plus_harmonics / (notes_count)) * 100:.2f}% "
-            f"({plus_harmonics}/{notes_count}) harmonics\n"
-            f"{(plus_unrelated / (notes_count)) * 100:.2f}% "
-            f"({plus_unrelated}/{notes_count}) unrelated\n")
+def print_unmatched_note_case(expected_note, info):
+  if info[0] == -1:
+    print(f"- {expected_note}: unrelated")
+  elif info[0] == 0:
+    print(f"- {expected_note}: duplicate")
+  elif info[0] == 0.5:
+    print(f"- {expected_note}: note match with {info[3]} cent difference")
+  else:
+    print(f"- {expected_note}: {info[3][0]}. harmonic of {info[3][1]}")
 
 # --------------------------------
 # |Just trying the functions here|
 # --------------------------------
 
-c4_x = m21.pitch.Pitch('c4')
-c4_x.microtone = 20
+c_20cent = m21.pitch.Pitch('c4')
+c_20cent.microtone = 20
+notes_ceg = [m21.pitch.Pitch('c4'), m21.pitch.Pitch('e4'), m21.pitch.Pitch('g4')]
+notes_ceg20 = [c_20cent, m21.pitch.Pitch('e4'), m21.pitch.Pitch('g4')]
+notes_cec = [m21.pitch.Pitch('c4'), m21.pitch.Pitch('e4'), m21.pitch.Pitch('c5')]
+notes_ace = [m21.pitch.Pitch('a3'), m21.pitch.Pitch('c3'), m21.pitch.Pitch('e3')]
 
-c4_y = m21.pitch.Pitch('c4')
+# compare_notes_n_to_n(notes_ceg, notes_ceg)
+# print("----------------------------------")
+# compare_notes_n_to_n(notes_ceg, notes_cec)
+# print("----------------------------------")
+# compare_notes_n_to_n(notes_ceg, notes_ace)
 
-g5 = m21.pitch.Pitch('g5')
-
-notes_x = [c4_x]
-notes_y = [c4_x, c4_y]
-notes_z = [g5]
-notes_w = [m21.pitch.Pitch('c2'), m21.pitch.Pitch('g5'), m21.pitch.Pitch('a6')]
-
-compare_notes(notes_x, notes_x)
-print("----------------------")
-compare_notes(notes_x, notes_y)
-print("----------------------")
-compare_notes(notes_x, notes_z)
-print("----------------------")
-compare_notes(notes_x, notes_w)
+rel_matrix = compare_notes(notes_ceg, notes_cec)
+print("------------------------------")
+rel_points_matrix = get_relationship_points(rel_matrix)
+print("------------------------------")
+scenarios = get_scenarios(rel_points_matrix)
+for key, value in scenarios.items():
+    print(key, ':', value)
