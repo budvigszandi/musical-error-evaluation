6fc9a4a20a34461833965e1fcd6f4124bcb4246d
Fajtaine Budvig Alexandra <alexandra.budvig@gmail.com>
2022-03-08 08:17:28 +0100

    Python beautifying dtw algorithm

--- metrics/rhythms/distances.py
+++ metrics/rhythms/distances.py
@@ -80,25 +80,25 @@ def get_all_step_permutations(source, target):
 
 def dtw(s, t, window):
   n, m = len(s), len(t)
-  w = np.max([window, abs(n-m)])
-  dtw_matrix = np.zeros((n+1, m+1))
+  w = np.max([window, abs(n - m)])
+  dtw_matrix = np.zeros((n + 1, m + 1))
   
-  for i in range(n+1):
-      for j in range(m+1):
-          dtw_matrix[i, j] = np.inf
+  for i in range(n + 1):
+    for j in range(m + 1):
+      dtw_matrix[i, j] = np.inf
   dtw_matrix[0, 0] = 0
   
-  for i in range(1, n+1):
-      for j in range(np.max([1, i-w]), np.min([m, i+w])+1):
-          dtw_matrix[i, j] = 0
+  for i in range(1, n + 1):
+    for j in range(np.max([1, i - w]), np.min([m, i + w]) + 1):
+      dtw_matrix[i, j] = 0
   
-  for i in range(1, n+1):
-      for j in range(np.max([1, i-w]), np.min([m, i+w])+1):
-          # cost = abs(s[i-1] - t[j-1])
-          cost = abs(get_rhythmic_distance(s[i-1], t[j-1]))
-          # take last min from a square box
-          last_min = np.min([dtw_matrix[i-1, j],     # insertion
-                             dtw_matrix[i, j-1],     # deletion
-                             dtw_matrix[i-1, j-1]])  # match
-          dtw_matrix[i, j] = cost + last_min
+  for i in range(1, n + 1):
+    for j in range(np.max([1, i - w]), np.min([m, i + w]) + 1):
+      # cost = abs(s[i - 1] - t[j - 1])
+      cost = abs(get_rhythmic_distance(s[i - 1], t[j - 1]))
+      # take last min from a square box
+      last_min = np.min([dtw_matrix[i - 1, j],       # insertion
+                         dtw_matrix[i, j - 1],       # deletion
+                         dtw_matrix[i - 1, j - 1]])  # match
+      dtw_matrix[i, j] = cost + last_min
   return dtw_matrix
