2aa868c179330b699533f8ca6e84cc479df39f99
Fajtaine Budvig Alexandra <alexandra.budvig@gmail.com>
2022-03-29 15:24:06 +0200

    Finished compressed DTW idea
    The idea was nice, however, it is still too slow to go through all step permutations

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from visualizer.draw_rhythmic_results import *
 from metrics.rhythms.evaluate_rhythms import *
 from input.midi_reader import *
 from visualizer.draw_harmonic_part_results import *
+from metrics.distances.compressed_dtw import *
 
 # ------------------------------
 # Expected and given note arrays
@@ -152,9 +153,9 @@ given_rhythm =    [c_half,    c_quarter, c_quarter, rest_quarter, c_quarter]
 # Expected and given melodies
 # ---------------------------
 
-score = get_score_from_midi("../midi/sna-short-multinote.mid")
+score = get_score_from_midi("../midi/melody-sample-sevennationarmy-onenote.mid")
 simplified_data = get_simplified_data_from_score(score)
-score_multinote = get_score_from_midi("../midi/sna-short-onenote.mid")
+score_multinote = get_score_from_midi("../midi/melody-sample-sevennationarmy-multinote.mid")
 simplified_data_multinote = get_simplified_data_from_score(score_multinote)
 
 # score = get_score_from_midi("../midi/sna-short-onenote.mid")
@@ -169,32 +170,57 @@ given_harmonic_part    = simplified_data_multinote
 # Melody evaluation
 # -----------------
 
-put_sheet_in_output_folder(score)
+# put_sheet_in_output_folder(score)
 
-print(simplified_data)
-print(simplified_data_multinote)
-dtw_matrix = dtw(simplified_data, simplified_data_multinote, 0, True)
-print(dtw_matrix)
+# print(simplified_data)
+# print(simplified_data_multinote)
+# dtw_matrix = dtw(simplified_data, simplified_data_multinote, 2, True)
+# print(dtw_matrix)
 
-all_step_permutations = get_all_step_permutations(expected_harmonic_part, given_harmonic_part)
-print("Got all step permutations")
-converted_permutations_dtw, points, note_evaluations = convert_steps_with_points_dtw(all_step_permutations, expected_harmonic_part, given_harmonic_part, dtw_matrix, True)
-print("Converted steps, got points")
+# all_step_permutations = get_all_step_permutations(expected_harmonic_part, given_harmonic_part)
+# print("Got all step permutations")
+# converted_permutations_dtw, points, note_evaluations = convert_steps_with_points_dtw(all_step_permutations, expected_harmonic_part, given_harmonic_part, dtw_matrix, True)
+# print("Converted steps, got points")
 
-# print("All permutations:")
-# for i in range(len(converted_permutations_dtw)):
-#   print(i + 1)
-#   draw_harmonic_part_differences_from_steps(expected_harmonic_part, given_harmonic_part, converted_permutations_dtw[i], note_evaluations[i])
-#   print("Point:", points[i])
+# # print("All permutations:")
+# # for i in range(len(converted_permutations_dtw)):
+# #   print(i + 1)
+# #   draw_harmonic_part_differences_from_steps(expected_harmonic_part, given_harmonic_part, converted_permutations_dtw[i], note_evaluations[i])
+# #   print("Point:", points[i])
+# #   print()
+
+# best_permutation_indices = get_best_permutation_indices(points)
+# print("Got best permutation indices")
+
+# print("Best permutation(s):\n")
+# for index in best_permutation_indices:
+#   draw_harmonic_part_differences_from_steps(expected_harmonic_part, given_harmonic_part, converted_permutations_dtw[index], note_evaluations[index])
+#   print("Point:", points[index])
 #   print()
 
-best_permutation_indices = get_best_permutation_indices(points)
-print("Got best permutation indices")
+# print(dtw_matrix)
 
-print("Best permutation(s):\n")
-for index in best_permutation_indices:
-  draw_harmonic_part_differences_from_steps(expected_harmonic_part, given_harmonic_part, converted_permutations_dtw[index], note_evaluations[index])
-  print("Point:", points[index])
-  print()
-
+# -----------------
+# Compressed DTW
+# -----------------
+n = len(simplified_data)
+m = len(simplified_data_multinote)
+window = 2
+constraint = max(window, abs(n - m))
+print("n", n, "m", m, "constraint", constraint)
+print("dtw size", n + 1, "x", m + 1)
+dtw_matrix = dtw(simplified_data, simplified_data_multinote, constraint, True)
 print(dtw_matrix)
+
+compressed_dtw = get_compressed_dtw(dtw_matrix, constraint)
+print("\ncompressed")
+print(compressed_dtw)
+
+print("step permutations for", compressed_dtw.shape[0], "rows", compressed_dtw.shape[1], "columns")
+count = 0
+step_permutations = get_all_step_permutations(compressed_dtw.shape[0], compressed_dtw.shape[1])
+print(step_permutations)
+for i in step_permutations:
+  for j in i:
+    count += 1
+print(count)
