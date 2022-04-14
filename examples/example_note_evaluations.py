99240854ebae3757e8c1fe212a76212e4d7a4698
Fajtaine Budvig Alexandra <alexandra.budvig@gmail.com>
2022-04-14 17:54:06 +0200

    Added more note evaluation examples

--- examples/example_note_evaluations.py
+++ examples/example_note_evaluations.py
@@ -32,6 +32,26 @@ def example_note_evaluation_harmonic():
   given_notes = [m21.pitch.Pitch('c4'), m21.pitch.Pitch('e4'), m21.pitch.Pitch('c5')]
   example_get_note_evaluation(expected_notes, given_notes)
 
+def example_note_evaluation_cent_difference():
+  expected_notes = [m21.pitch.Pitch('c4'), m21.pitch.Pitch('e4'), m21.pitch.Pitch('g4')]
+  given_notes = [m21.pitch.Pitch('c4', microtone=20), m21.pitch.Pitch('e4'), m21.pitch.Pitch('g5')]
+  example_get_note_evaluation(expected_notes, given_notes)
+
+def example_note_evaluation_expected_one_got_more():
+  expected_notes = [m21.pitch.Pitch('c4')]
+  given_notes = [m21.pitch.Pitch('c3'), m21.pitch.Pitch('c5'), m21.pitch.Pitch('c6')]
+  example_get_note_evaluation(expected_notes, given_notes)
+
+def example_note_evaluation_expected_more_got_one():
+  expected_notes = [m21.pitch.Pitch('c4'), m21.pitch.Pitch('e4'), m21.pitch.Pitch('g4')]
+  given_notes = [m21.pitch.Pitch('e6')]
+  example_get_note_evaluation(expected_notes, given_notes)
+
+def example_note_evaluation_got_only_unrelated():
+  expected_notes = [m21.pitch.Pitch('c4'), m21.pitch.Pitch('e4'), m21.pitch.Pitch('g4')]
+  given_notes = [m21.pitch.Pitch('a2'), m21.pitch.Pitch('c3'), m21.pitch.Pitch('e3')]
+  example_get_note_evaluation(expected_notes, given_notes)
+
 def example_note_evaluation_expected_none_got_some():
   expected_notes = []
   given_notes = [m21.pitch.Pitch('c4'), m21.pitch.Pitch('e4'), m21.pitch.Pitch('g4')]
