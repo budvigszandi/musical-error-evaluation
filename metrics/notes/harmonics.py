096ac4a59fe028e1e97d74cec7523e9ac41383c9
Fajtaine Budvig Alexandra <alexandra.budvig@gmail.com>
2022-04-16 14:58:23 +0200

    Deleted unnecessary harmonic functions

--- metrics/notes/harmonics.py
+++ metrics/notes/harmonics.py
@@ -5,16 +5,6 @@ import music21 as m21
 # Simple will reduce an octave to a unison, semiSimple treats octaves as
 # distinct intervals (P8.simple: Perfect Unison; P8.semiSimple: Perfect Octave)
 
-# Returns an array of pitches which is the harmonic series from the
-# 1st harmonic (fundamental note) to the 16th harmonic
-#
-# Requires an m21.pitch.Pitch object
-def build_harmonic_series(fundamental_note):
-  harmonic_series = [fundamental_note]
-  for i in range(2, 17):
-    harmonic_series.append(fundamental_note.getHarmonic(i))
-  return harmonic_series
-
 # Returns a boolean about whether the given note is the first harmonic of the
 # supposedly fundamental note (in other words, they are the same note
 # or enharmonically equal). This is necessary because music21's
@@ -59,19 +49,3 @@ def get_harmonic_info(note, fundamental_note):
   else:
     #print('Cannot find an equivalent harmonic for a fundamental', fundamental_note, 'that would be', note)
     return 0
-
-# Returns a number between 0 and 1 regarding how probable it is that
-# we have misheard a note to one of its harmonics based on the
-# variance (which we have in cents)
-#
-# Requires a number which represents which harmonic we are looking for
-# (from 1 and 16)
-def probabilty_of_harmonic(harmonic_number):
-  example_harmonic_series = build_harmonic_series(m21.pitch.Pitch('C4'))
-  variance = example_harmonic_series[harmonic_number - 1].microtone.cents
-  if harmonic_number == 1:
-    return 0.99
-  if variance > 0:
-    return 1 / (harmonic_number * variance)
-  else:
-    return 1 / harmonic_number
